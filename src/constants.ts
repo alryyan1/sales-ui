feb3ec15bbb19660c15be4f209c2def0feccc161
Ashjan <ashjan.zubeir@gmail.com>
2026-05-11 07:23:52 +0200

    update auditPdf

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // export const host = 'sahara-pharma.com'
-export const schema = "https";
+export const schema = "http";
 // export const host = "lifcaresd.com";
 // export const host = "backend.lifcaresd.com";
 //export const host = "localhost";
